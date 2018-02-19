27b41bc8e5663458c3f26a5245fb786523a315d2
Stefan Klas <sk15962@bristol.ac.uk>
2018-02-19 19:05:42 +0000

    Removed VR library- not needed right now

--- Assets/OVR/Scripts/OVRCameraRig.cs
+++ Assets/OVR/Scripts/OVRCameraRig.cs
@@ -155,6 +155,7 @@ public class OVRCameraRig : MonoBehaviour
         rightHandAnchor.localRotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch);
 
 		trackerAnchor.localPosition = tracker.position;
+     
 #if UNITY_2017_2_OR_NEWER
 		centerEyeAnchor.localPosition = UnityEngine.XR.InputTracking.GetLocalPosition(UnityEngine.XR.XRNode.CenterEye);
 		leftEyeAnchor.localPosition = monoscopic ? centerEyeAnchor.localPosition : UnityEngine.XR.InputTracking.GetLocalPosition(UnityEngine.XR.XRNode.LeftEye);
