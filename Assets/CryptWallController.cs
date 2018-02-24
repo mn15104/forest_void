9a1275cbd17a521b07ce4aaac81aa32415e3f9ed
Alessio Zakaria <alessio.b.zakaria@gmail.com>
2018-02-24 20:56:27 +0000

    Added wall trigger for the crypt

---
+++ Assets/CryptWallController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CryptWallController : MonoBehaviour {
+
+    public List<GameObject> toTurnOn;
+    public List<GameObject> toTurnOff;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void TurnWallsOff()
+    {
+        foreach (GameObject wall in toTurnOff)
+        {
+            if (wall.GetComponent<MeshRenderer>())
+            {
+                Debug.Log("Fuck");
+                wall.GetComponent<MeshRenderer>().enabled = false;
+                if (wall.GetComponent<MeshCollider>())
+                {
+                    wall.GetComponent<MeshCollider>().enabled = false;
+                }
+            }
+        }
+    }
+
+    private void TurnWallsOn()
+    {
+        foreach (GameObject wall in toTurnOn)
+        {
+            if (wall.GetComponent<MeshRenderer>())
+            {
+                wall.GetComponent<MeshRenderer>().enabled = true;
+                if (wall.GetComponent<MeshCollider>())
+                {
+                    wall.GetComponent<MeshCollider>().enabled = true;
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        TurnWallsOff();
+        TurnWallsOn();
+    }
+}
