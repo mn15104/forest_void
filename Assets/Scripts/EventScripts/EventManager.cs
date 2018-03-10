dc08ccc881f647d5222dbc06306bd731be00da6a
Aatina Punjabi <aatina@hotmail.co.uk>
2018-03-10 12:40:47 +0000

    Added (broken) event manager

---
+++ Assets/Scripts/EventScripts/EventManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour {
+
+	public delegate void TextAction();
+	public static event TextAction ViewText;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (ViewText != null) {
+			Debug.Log("View text is not null");
+			Debug.Log (ViewText);
+			ViewText();
+		}
+	}
+}
--- Assets/Scripts/EventScripts/Text Scripts/TextController.cs
+++ Assets/Scripts/EventScripts/Text Scripts/TextController.cs
@@ -59,20 +59,48 @@ public class TextController : MonoBehaviour {
         Debug.Log("Back to transparent");
     }
 
+	void FadeInDelegate()
+	{
+		Debug.Log("Fading");
+		FadingIn = true;
+		float timeToStart = Time.time;
+		while (text.color != new Color(1, 1, 1, 1))
+		{
+			text.color = Color.Lerp(text.color, new Color(1, 1, 1, 1), (Time.time - timeToStart) * 0.005f);
+		}
+		Debug.Log("Changed Colour");
+		FadingIn = false;
+	}
+
+	void FadeOutDelegate(){
+		while (FadingIn) {
+			Debug.Log("Waiting for fade in to finish");
+		}
+		float timeToStart = Time.time;
+		while (text.color != new Color(1, 1, 1, 0))
+		{
+			text.color = Color.Lerp(text.color, new Color(1, 1, 1, 0), (Time.time - timeToStart) * 0.005f);
+		}
+		Debug.Log("Back to transparent");
+	}
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject == ViewCone)
         {
             Debug.Log("Fading In");
-            StartCoroutine("FadeIn");
+            //StartCoroutine("FadeIn");
+			EventManager.ViewText += FadeInDelegate;
         }
     }
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject == ViewCone)
-        {
-            StartCoroutine("FadeOut");
-        }
-    }
+//    private void OnTriggerExit(Collider other)
+//    {
+//        if (other.gameObject == ViewCone)
+//        {
+//            //StartCoroutine("FadeOut");
+//			EventManager.ViewText -= FadeInDelegate;
+//			EventManager.ViewText += FadeOutDelegate;
+//        }
+//    }
 }
