9c2c3a85f77d144ad68a39cd5597ccd22ee13138
aatina <aatina@hotmail.co.uk>
2018-03-13 19:47:18 +0000

    Key with snap rotation in

--- Assets/Scripts/HumanScripts/VR/RetreiveKey.cs
+++ Assets/Scripts/HumanScripts/VR/RetreiveKey.cs
@@ -97,7 +97,7 @@ public class RetreiveKey : OVRGrabber
     private IEnumerator AddKeyToInventory(OVRGrabbable m_grabbedObj)
     {
      
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(10f);
         if (m_grabbedObj != null)
         {
 
