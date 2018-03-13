24b1b3ce9ad16aa4155b3d5442532923e9de3e7c
aatina <aatina@hotmail.co.uk>
2018-03-13 12:17:43 +0000

    Flickering generator light

--- Assets/FlickeringLight.cs
+++ Assets/FlickeringLight.cs
@@ -2,42 +2,63 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FlickeringLight : MonoBehaviour {
+public class FlickeringLight : MonoBehaviour
+{
 
     private Light f_Light;
-    private float timer;
+    private float currentTimer;
+    public float maxTimer;
+    public float maxTimeDiff;
+    public float lowerBound;
+    public float setLowIntensity;
+    public float setHighIntensity;
+    public float higherBound;
+    private float highIntensity;
+    private float lowIntensity;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
+        //setlowIntensity = 1f;
+        //sethighIntensity = 3f;
+        //lowIntensity = 1f;
+        //highIntensity = 3f;
+        //lowerBound = 0.5f;
+        //higherBound = 4f;
+        //maxTimeDiff = 2f;
+        //maxTimer = 5f;
         f_Light = gameObject.GetComponent<Light>();
-        f_Light.intensity = 3;
+        f_Light.intensity = highIntensity;
+        
     }
 
 
-	// Update is called once per frame
-	void Update () {
-        int chance = Random.Range(0, 101);
-        Debug.Log("hi");
-        if (chance < 80)
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentTimer > maxTimer)
         {
-            f_Light.intensity = 3;
+            int chance = Random.Range(0, 101);
+            if (chance < 90)
+            {
+                f_Light.intensity = highIntensity;
+                currentTimer = 0;
+                maxTimer = Random.Range(0, maxTimeDiff);
+                highIntensity = Random.Range(setHighIntensity, higherBound);
+                lowIntensity = Random.Range(lowerBound, setLowIntensity);
+                
+            }
         }
         else
         {
-            f_Light.intensity = 1;
-            timer = 5.0f;
-            while (timer > 0)
-            {
-                timer = timer - Time.time;
-            }
-           
+            f_Light.intensity = lowIntensity;
+            currentTimer += Time.deltaTime;
         }
-        
-		
-	}
 
-    //IEnumerator timer()
-    //{
-    //    yield return new WaitForSeconds(5);
-    //}
+        //IEnumerator timer()
+        //{
+        //    Debug.Log("running couroutine");
+        //    yield return WaitForSeconds(5);
+        //}
+    }
 }
