112e09f3b98137cbef17cafe64f5c18a7a6f7699
aatina <aatina@hotmail.co.uk>
2018-03-14 17:48:33 +0000

    Backdoor trigger added

---
+++ Assets/BackDoorTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackDoorTrigger : MonoBehaviour {
+
+    private GameObject handCollider;
+
+    private void Start()
+    {
+        handCollider = GameObject.FindWithTag("HandCollider");
+        handCollider.SetActive(false);
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            handCollider.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            handCollider.SetActive(false);
+        }
+    }
+}
