dc082fc00da33d5f99210e262db7ec2f419c1f5d
mn15104 <mn15104@my.bris.ac.uk>
2018-03-31 01:02:14 +0100

    Finally cleaned everything up

---
+++ Assets/Scripts/VoidScripts/VoidNamespace.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+//namespace VoidNamespace
+//{
+//    class VoidNamespace
+//    {
+//        partial class MonsterAI : MonoBehaviour { };
+//        partial class VoidSystem : MonoBehaviour { };
+//        partial class MonsterAIState { };
+//    }
+//}
