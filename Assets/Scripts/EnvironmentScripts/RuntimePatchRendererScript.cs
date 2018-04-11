344e10ba73b63568de2c1c1fff30d469d450f148
Stefan Klas <sk15962@bristol.ac.uk>
2018-04-11 12:16:54 +0100

    turned off runtime patch rendering

---
+++ Assets/Scripts/EnvironmentScripts/RuntimePatchRendererScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuntimePatchRendererScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Terrain.activeTerrain.collectDetailPatches = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
