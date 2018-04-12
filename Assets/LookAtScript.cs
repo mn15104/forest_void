40d619fd93e20c99e894500193624c1cae67b550
Stefan Klas <sk15962@bristol.ac.uk>
2018-04-12 16:55:18 +0100

    rotate tracking space added

---
+++ Assets/LookAtScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtScript : MonoBehaviour {
+
+    public TrackingSpaceController trackingSpace;
+    public bool LookBack = false;
+    public float rotateSpeed = 5f;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if(LookBack)
+        {
+            trackingSpace.VRLookAT(transform, rotateSpeed);
+            LookBack = false;
+        }
+	}
+}
