872f6c76218dffda01f577c13ca3f232fbe8660e
Stefan Klas <sk15962@bristol.ac.uk>
2018-04-16 13:09:14 +0100

    added new ovr human and turned on rendering and camera settings

---
+++ Assets/Scripts/HumanScripts/VR/TrackingSpaceController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackingSpaceController : MonoBehaviour {
+
+    public float CurrentRotateTime;
+
+    private Quaternion previousLocation;
+    private Transform ToBeLookedAt;
+
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void VRLookAT(Transform toBeLookedAt, float time)
+    {
+        CurrentRotateTime = time;
+        ToBeLookedAt = toBeLookedAt;
+        StartCoroutine("VrLooking");
+    }
+
+    IEnumerator VrLooking()
+    {
+        float elapsedTime = 0f;
+        var neededRotation = Quaternion.LookRotation(ToBeLookedAt.position - transform.position);
+        while((neededRotation != transform.rotation) && elapsedTime < 5f)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, neededRotation, Time.deltaTime * CurrentRotateTime);
+            elapsedTime += Time.deltaTime;
+            Debug.Log(elapsedTime);
+            yield return null;
+        }
+        Debug.Log("finished looking at");
+    }
+
+    public void VRLookBack()
+    {
+        transform.rotation = previousLocation;
+    }
+}
