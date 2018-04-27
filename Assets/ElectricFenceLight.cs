e98fcb6c26448c1e055d4a60b7392f400dc8656e
Stefan Klas <sk15962@bristol.ac.uk>
2018-04-27 17:30:11 +0100

    Electric fence opening, light change + plus electric fence disappears

---
+++ Assets/ElectricFenceLight.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectricFenceLight : MonoBehaviour {
+
+    public GameObject goLight;
+    public GameObject stopLight;
+    protected EventManager eventManager;
+
+
+    // Use this for initialization
+    void Start () {
+        stopLight.GetComponent<Light>().enabled = true;
+        goLight.GetComponent<Light>().enabled = false;
+        eventManager = FindObjectOfType<EventManager>();
+        eventManager.NotifyAllKeysInserted.NotifyEventOccurred += switchToGoLight;
+    }
+
+    void switchToGoLight(bool value)
+    {
+        stopLight.GetComponent<Light>().enabled = false;
+        goLight.GetComponent<Light>().enabled = true;
+    }   
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/ElectricFenceText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ElectricFenceText : MonoBehaviour {
+
+    private EventManager eventManager;
+
+    // Use this for initialization
+    void Start () {
+        eventManager = FindObjectOfType<EventManager>();
+        eventManager.NotifyAllKeysInserted.NotifyEventOccurred += TextDisappear;
+    }
+
+    void TextDisappear(bool value) {
+        GetComponent<Text>().enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
